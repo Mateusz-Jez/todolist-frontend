9b98a9c81854ccb3de5d5fb0cab7db3923e53c59
Mateusz-Jez <228756@edu.p.lodz.pl>
2022-07-19 16:02:15 +0200

    deleting and completing tasks

--- src/todolist/todolist.controller.js
+++ src/todolist/todolist.controller.js
@@ -1,13 +1,59 @@
 angular.module('ToDoListApp.controller', [])
 .controller('ToDoListController', function($scope, $routeParams, todolistAPIservice) {
     $scope.tasks = [];
+    $scope.selectedTasksId = [];
+    $scope.notCompletedSelected = false;
 
     todolistAPIservice.getTasks($routeParams.category).then(function (data) {
        $scope.tasks = data;
     });
-}).
-controller('AddTaskController', function($scope, todolistAPIservice) {
-    $scope.addTask = function(){
-        todolistAPIservice.addTask($scope.title, $scope.description, $scope.deadline);
+
+    $scope.selectTask = function (id) {
+        var idInArray = $scope.selectedTasksId.indexOf(id)
+        if(idInArray === -1) {
+            $scope.selectedTasksId.push(id);
+        }
+        else {
+            $scope.selectedTasksId.splice(idInArray, 1);
+        }
+        $scope.checkIfNotCompletedSelected();
+    }
+
+    $scope.deleteTasks = function () {
+        todolistAPIservice.deleteTasks($scope.selectedTasksId);
+    }
+
+    $scope.checkIfNotCompletedSelected = function () {
+        $scope.notCompletedSelected = false;
+        angular.forEach($scope.tasks, function (value, key) {
+            if($scope.selectedTasksId.indexOf(value.id) !== -1) {
+                if(value.taskCategory !== 'COMPLETED') {
+                    $scope.notCompletedSelected = true;
+                }
+            }
+        });
+    }
+
+    $scope.markTasksAsCompleted = function () {
+        todolistAPIservice.markTasksAsCompleted($scope.selectedTasksId);
+    }
+
+    $scope.editTask = function () {
+
+    }
+})
+.controller('AddTaskController', function($scope, todolistAPIservice, $window) {
+    $scope.checkDate = function () {
+        if($scope.deadline <= new Date()) {
+            $scope.error = "Please enter a future date!";
+        }
+        else {
+            delete $scope.error;
+            $scope.addTask();
+        }
+    }
+
+    $scope.addTask = function (){
+        todolistAPIservice.addTask($scope.title, $scope.description, $scope.deadline, $window);
     }
 });
--- src/todolist/todolist.service.js
+++ src/todolist/todolist.service.js
@@ -12,7 +12,7 @@ angular.module('ToDoListApp.service', [])
         });
     }
 
-    todolistAPI.addTask = function (title, description, deadline) {
+    todolistAPI.addTask = function (title, description, deadline, window) {
 
         return $http({
             method: 'POST',
@@ -23,8 +23,34 @@ angular.module('ToDoListApp.service', [])
                 "deadline": deadline,
                 "taskCategory": "PENDING"
             }
+        }).then(function (){
+            if(!alert('Successfully added new task!')){
+                window.location.reload();
+            }
         });
     }
+
+    todolistAPI.deleteTasks = function (tasks) {
+        return $http({
+                    method: 'DELETE',
+                    url: "http://localhost:8080/task",
+                    data : tasks
+                }).then(function (){
+                    if(!alert('Selected tasks have been deleted!')){
+                    window.location.reload();
+                    }
+                });
+    }
+
+    todolistAPI.markTasksAsCompleted = function (tasks) {
+            return $http({
+                        method: 'POST',
+                        url: "http://localhost:8080/task/complete",
+                        data : tasks
+                    }).then(function (){
+                        window.location.reload();
+                    });
+        }
 
     return todolistAPI;
 });
